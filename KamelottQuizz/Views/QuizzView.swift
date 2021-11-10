f5c8d59d3d6e34c46e15b0ac82f590990baa6a4c
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-10 15:25:19 +0800

    Add quizz view

---
+++ KamelottQuizz/Views/QuizzView.swift
@@ -0,0 +1,22 @@
+//
+//  QuizzView.swift
+//  KamelottQuizz
+//
+//  Created by Giovanni Gaffé on 2021/11/10.
+//
+
+import SwiftUI
+
+struct QuizzView: View {
+    var body: some View {
+        VStack {
+            Text("Hello world")
+        }
+    }
+}
+
+struct QuizzView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuizzView()
+    }
+}
