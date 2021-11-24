06ffe116605313c74923534a24e8b23c7d21cd41
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-24 16:10:34 +0800

    Set game model

---
+++ KamelottQuizz/Model/Game.swift
@@ -0,0 +1,84 @@
+//
+//  Game.swift
+//  KamelottQuizz
+//
+//  Created by Giovanni Gaffé on 2021/11/24.
+//
+
+import Foundation
+
+class Game: ObservableObject {
+    @Published var questionAmount = "0"
+    @Published var challengeMode = false
+    var randomCharacters = Game().pickRandomCharacters()
+    
+    let characters = [
+        "Angharad",
+        "Anna",
+        "Appius Manilius",
+        "Arthur",
+        "Attila",
+        "Belt",
+        "Père Blaise",
+        "Bohort",
+        "Breccan",
+        "Le Roi Burgonde",
+        "Caius Camillus",
+        "Calogrenant",
+        "Capito",
+        "César",
+        "Cryda de Tintagel",
+        "Dagonet",
+        "La Dame du Lac",
+        "Demetra",
+        "Drusilla",
+        "Le Duc d'Aquitaine",
+        "Edern",
+        "Elias de Kelliwic'h",
+        "Galessin",
+        "Gauvain",
+        "Goustan",
+        "Grüdü",
+        "Guenièvre",
+        "Guethenoc",
+        "Hervé de Rinel",
+        "L'interprète burgonde",
+        "Le Seigneur Jacca",
+        "Les Jumelles du pêcheur",
+        "Le Jurisconsulte",
+        "Kadoc",
+        "Karadoc",
+        "Lancelot",
+        "Léodagan",
+        "Loth",
+        "Le Maître d'Armes",
+        "Méléagant",
+        "Manius Macrinus Firmus",
+        "Merlin",
+        "Mevanwi",
+        "Perceval",
+        "Roparzh",
+        "Lucius Sillius Sallustius",
+        "Séfriane d'Aquitaine",
+        "Séli",
+        "Spurius Cordius Frontinius",
+        "Le Tavernier",
+        "Urgan",
+        "Vérinus",
+        "Venec",
+        "Ygerne",
+        "Yvain",
+    ]
+    
+    
+    /// Return an array with 2 random characters
+    func pickRandomCharacters() -> [String] {
+        var tempCharacters = [String]()
+        
+        for _  in 0...1 {
+            tempCharacters.append(characters[Int.random(in: 0...characters.count)])
+        }
+        
+        return tempCharacters
+    }
+}
