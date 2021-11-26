0ee91bd99d89116662ed8990efff950daf486ede
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-26 11:20:37 +0800

    set form view model with some citations

---
+++ KamelottQuizz/ViewModel/FormViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  FormViewModel.swift
+//  KamelottQuizz
+//
+//  Created by Giovanni Gaffé on 2021/11/26.
+//
+
+import Foundation
+
+final class FormViewModel : ObservableObject {
+
+    @Published var citationRepresentable: Citation?
+
+
+}
