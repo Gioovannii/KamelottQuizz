c3915388b70cf07b7888ad66f96305e419a4249d
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-26 11:28:37 +0800

    remove load data from quizz should be load at launch

--- KamelottQuizz/ViewModel/QuizzViewModel.swift
+++ KamelottQuizz/ViewModel/QuizzViewModel.swift
@@ -10,36 +10,15 @@ import Foundation
 final class QuizzViewModel : ObservableObject {
     
     @Published var citationRepresentable: Citation?
-    @Published  var buttonName = "name"
-    @Published  var correctResponse = ""
-    @Published var personnages = [""]
-
+    @Published var buttonName = "name"
+    @Published var correctResponse = ""
+    @Published var characters = [String]()
+    
     let questionAmount: String
     let challengeMode: Bool
     
     init(questionAmount: String, challengeMode: Bool) {
         self.questionAmount = questionAmount
         self.challengeMode = challengeMode
-    }
-    
-    // load data from url
-    func loadData() {
-        guard let url = URL(string: "https://kaamelott.chaudie.re/api/random") else {
-            print("Invalid url")
-            return
-        }
-        
-        let request = URLRequest(url: url)
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data {
-                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                    DispatchQueue.main.async {
-                        self.citationRepresentable = decodedResponse.citation
-                    }
-                    return
-                }
-            }
-            print("Fetched failed: \(error?.localizedDescription ?? "Unknown error")")
-        }.resume()
     }
 }
