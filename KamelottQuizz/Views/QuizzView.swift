6bfb32ec36f784952cece7949b0f2007a755357a
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-28 21:50:06 +0800

    refactor quizz view

--- KamelottQuizz/Views/QuizzView.swift
+++ KamelottQuizz/Views/QuizzView.swift
@@ -8,85 +8,93 @@
 import SwiftUI
 
 struct QuizzView: View {
-
+    
     @Environment(\.presentationMode) var presentationMode
     @StateObject var viewModel: QuizzViewModel
     
     let game = Game()
     
-    @State var citationRepresentable: Citation?
-    @State var currentQuestion = 1
-    @State var correctResponse = ""
-    @State var score = 0
-    @State var showingAlert = false
-
+    
+    
     
     var body: some View {
-            VStack {
-                Text("Score \(score)")
-                
-                Text("Question no \(currentQuestion) on \(viewModel.questionAmount)")
-                    .font(.largeTitle)
-                    .padding()
-                
-                Section(header: Text("Citation")) {
-                    if let citationRepresentable = citationRepresentable {
-                        Text(citationRepresentable.citation)
-                            .padding()
-                        Text(citationRepresentable.infos.personnage)
-                    } else { Text("Error with the citation ") }
-                }
-                
-                Spacer()
-                
-                Section {
-                    Text("Qui a dit cette citation")
-                        .font(.headline)
-                    ForEach(0 ..< 3) { number in
-                        Button(action:  {
-                            // do something when tapped
-                           
-                            
-                        })  {
-                            Text("\(number)")}
-                        
-                    }
-                }
+        VStack {
+            Text("Question no \(viewModel.currentQuestion) on \(viewModel.questionAmount)")
+                .font(.largeTitle)
                 .padding()
-                
-                Spacer()
-                
-                HStack {
-                    Button {
-                        // When pressed stop
-                        showingAlert.toggle()
-//                        self.presentationMode.wrappedValue.dismiss()
-                    } label: {
-                        Label("Stop", systemImage: "stop.circle.fill")
-                    }
-                    .alert(isPresented: $showingAlert, content: {
-                        Alert(title: Text("Attention"), message: Text("Tu veux t'arrêter là"), primaryButton: .cancel(Text("OK")), secondaryButton: .destructive(Text("Oh no"), action: quitGame))
-                    })
-                    .buttonStyle(.bordered)
-                    .tint(.red)
-                    
-                    Spacer()
-                    
-                    Button {
-                        //When pressed next question
-                        guard let questionAmount = Int(viewModel.questionAmount) else { return }
-                        guard currentQuestion < questionAmount else { return }
-                        self.currentQuestion += 1
-                        
-                    } label: {
-                        Label("Prochaine question", systemImage: "playpause.fill")
-                    }
-                    .disabled(viewModel.questionAmount == String(currentQuestion) ? true : false)
-                    .buttonStyle(.bordered)
-                    .tint(.blue)
-                }
-                .padding()
+            
+            Section(header: Text("Citation")) {
+                if let citationRepresentable = viewModel.citation {
+                    Text(citationRepresentable.citation)
+                        .padding()
+                    Text(citationRepresentable.infos.personnage)
+                } else { Text("Error with the citation ") }
             }
+            
+            Spacer()
+            
+            Section {
+                Text("Qui a dit cette citation")
+                    .font(.headline)
+                ForEach(0 ..< 3) { number in
+                    Button(action:  {
+                        // do something when tapped
+//                        if viewModel.c
+                    })  {
+                        if viewModel.characters.count > 1 {
+                            Text("\(viewModel.characters[number])")
+                        } else {
+                            Text("Loading")
+                        }
+                    }
+                }
+            }
+            .padding()
+            
+            Spacer()
+            
+            HStack {
+                Button {
+                    viewModel.showingAlert.toggle()
+                } label: {
+                    Label("Stop", systemImage: "stop.circle.fill")
+                }
+                .alert(isPresented: $viewModel.showingAlert) {
+                    Alert(title: Text("Attention"), message: Text("Tu veux t'arrêter là"), primaryButton: .cancel(Text("Je continue.")), secondaryButton: .destructive(Text("Oui c'est bon."), action: quitGame))
+                }
+                .buttonStyle(.bordered)
+                .tint(.red)
+                
+                Spacer()
+                
+                Button {
+                    guard let questionAmount = Int(viewModel.questionAmount) else { return }
+                    guard viewModel.currentQuestion < questionAmount else { return }
+                    self.viewModel.currentQuestion += 1
+                    
+                } label: {
+                    Label("Prochaine question", systemImage: "playpause.fill")
+                }
+                .disabled(viewModel.questionAmount == String(viewModel.currentQuestion) ? true : false)
+                .buttonStyle(.bordered)
+                .tint(.blue)
+            }
+            .padding()
+        }
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItemGroup(placement: .navigationBarLeading) {
+                Text("score \(viewModel.score)")
+                    .font(.headline)
+                    .fontWeight(.semibold)
+            }
+            
+            ToolbarItemGroup(placement: .navigationBarTrailing) {
+                Text(self.viewModel.challengeMode ? "Mode challenge activé" : "Mode challenge desactivé")
+                    .font(.headline)
+                    .fontWeight(.semibold)
+            }
+        }
     }
     
     
@@ -96,7 +104,6 @@ struct QuizzView: View {
 
 struct QuizzView_Previews: PreviewProvider {
     static var previews: some View {
-        QuizzView(viewModel: QuizzViewModel(questionAmount: "10", challengeMode: true))
-            .previewDevice("iPhone 13 Pro Max")
+        QuizzView(viewModel: QuizzViewModel(citation: CitationRepresentable.dumbCitation, questionAmount: "10", challengeMode: true, characters: ["Arthur", "Jean", "Paul"]))
     }
 }
