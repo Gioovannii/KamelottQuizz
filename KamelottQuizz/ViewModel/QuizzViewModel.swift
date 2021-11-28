75da2a4c220675ad83044a4b5bf2aa4fe4bf216d
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-28 21:48:49 +0800

    refactor quizz view model by passing a recipe representable

--- KamelottQuizz/ViewModel/QuizzViewModel.swift
+++ KamelottQuizz/ViewModel/QuizzViewModel.swift
@@ -9,16 +9,24 @@ import Foundation
 
 final class QuizzViewModel : ObservableObject {
     
-    @Published var citationRepresentable: Citation?
     @Published var buttonName = "name"
     @Published var correctResponse = ""
-    @Published var characters = [String]()
+    @Published var currentQuestion = 1
+    @Published var score = 0
+    @Published var showingAlert = false
+    
+    let citation: CitationRepresentable
     
     let questionAmount: String
     let challengeMode: Bool
+    let characters: [String]
+
     
-    init(questionAmount: String, challengeMode: Bool) {
+    init(citation: CitationRepresentable,
+         questionAmount: String, challengeMode: Bool, characters: [String]) {
         self.questionAmount = questionAmount
         self.challengeMode = challengeMode
+        self.characters = characters
+        self.citation = citation
     }
 }
