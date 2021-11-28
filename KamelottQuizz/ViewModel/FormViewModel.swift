5ef46ffe06c686af1efd25cd47a3dd94655db0cf
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-28 21:48:21 +0800

    remove load data

--- KamelottQuizz/ViewModel/FormViewModel.swift
+++ KamelottQuizz/ViewModel/FormViewModel.swift
@@ -48,25 +48,4 @@ final class FormViewModel : ObservableObject {
             print("Fetched failed: \(error?.localizedDescription ?? "Unknown error")")
         }.resume()
     }
-    
-    //    func loadData() {
-    //        guard let url = URL(string: "https://kaamelott.chaudie.re/api/random") else {
-    //            print("Invalid url")
-    //            return
-    //        }
-    //
-    //        let request = URLRequest(url: url)
-    //        URLSession.shared.dataTask(with: request) { data, response, error in
-    //            if let data = data {
-    //                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-    //                    DispatchQueue.main.async {
-    //                        self.citationRepresentable = decodedResponse.citation
-    //                        self.game.randomCharacters = self.game.pickRandomCharacters(answer: decodedResponse.citation.infos.personnage)
-    //                    }
-    //                    return
-    //                }
-    //            }
-    //            print("Fetched failed: \(error?.localizedDescription ?? "Unknown error")")
-    //        }.resume()
-    //    }
 }
