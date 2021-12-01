3b26cba4ecdf1b976a6755a17352ee779285b7d7
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-12-01 16:54:48 +0800

    set main view

---
+++ KamelottQuizz/Views/Launch/MainView.swift
@@ -0,0 +1,30 @@
+//
+//  MainView.swift
+//  KamelottQuizz
+//
+//  Created by Giovanni Gaffé on 2021/11/30.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            WelcomeView()
+                .tabItem {
+                    Label("Accueil", systemImage: "house.fill")
+                }
+            
+            FormView()
+                .tabItem {
+                    Label("Quizz", systemImage: "gamecontroller.fill")
+                }
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
