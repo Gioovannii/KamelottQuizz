31cc6d378fd7a01e75176f845642fbae59604db2
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-12-16 22:56:42 +0800

    indent quizz view and refactor

--- KamelottQuizz/ViewModel/QuizzViewModel.swift
+++ KamelottQuizz/ViewModel/QuizzViewModel.swift
@@ -7,34 +7,36 @@
 
 import Foundation
 
-final class QuizzViewModel : ObservableObject {
-    @Published var currentQuestion = 1
-    @Published var score = 0
-    
-    var wrappedQuestionAmount: Int {
-        Int(questionAmount) ?? 20
-    }
-    
-    let citations: [Citation]
-    let citation: Citation
-    
-    let questionAmount: String
-    let challengeMode: Bool
-    let characters: [[String]]
-    
-    let coreDM = CoreDataManager()
-    
-    init(citations: [Citation], citation: Citation,
-         questionAmount: String, challengeMode: Bool, characters: [[String]]) {
-        self.questionAmount = questionAmount
-        self.challengeMode = challengeMode
-        self.characters = characters
-        self.citation = citation
-        self.citations = citations
-    }
-    
-    func nextQuestion() {
-        guard currentQuestion < wrappedQuestionAmount else { return }
-        self.currentQuestion += 1
+extension QuizzView {
+    @MainActor class QuizzViewModel : ObservableObject {
+        @Published var currentQuestion = 1
+        @Published var score = 0
+        
+        var wrappedQuestionAmount: Int {
+            Int(questionAmount) ?? 20
+        }
+        
+        let citations: [Citation]
+        let citation: Citation
+        
+        let questionAmount: String
+        let challengeMode: Bool
+        let characters: [[String]]
+        
+        let coreDM = CoreDataManager()
+        
+        init(citations: [Citation], citation: Citation,
+             questionAmount: String, challengeMode: Bool, characters: [[String]]) {
+            self.questionAmount = questionAmount
+            self.challengeMode = challengeMode
+            self.characters = characters
+            self.citation = citation
+            self.citations = citations
+        }
+        
+        func nextQuestion() {
+            guard currentQuestion < wrappedQuestionAmount else { return }
+            self.currentQuestion += 1
+        }
     }
 }
