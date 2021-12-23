558e4d80512b797347f7c8341af4e15ac4a6dd12
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-12-23 16:23:48 +0800

    change frame for badge

--- KamelottQuizz/Helper + Utilities/Badges.swift
+++ KamelottQuizz/Helper + Utilities/Badges.swift
@@ -11,8 +11,7 @@ extension Image {
     func badgeImage(isUnlocked: Bool) -> some View {
         self
             .resizable()
-            .frame(width: 55, height: 55)
-            .padding(.horizontal)
+            .frame(width: 40, height: 40)
             .clipShape(RoundedRectangle(cornerRadius: 5))
             .opacity(isUnlocked ? 1.0 : 0.5)
             .saturation(isUnlocked ? 1.0 : 0.0)
